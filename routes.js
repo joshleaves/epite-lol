b51dfb0bf014abbd09b2c25d443597bcf3c1d3bc
red <red@dreamleaves.org>
2021-12-02 02:16:25 +0100

    chore: Make code good

--- routes.js
+++ routes.js
@@ -1,29 +1,43 @@
-(function () {
-    var strings = require('./strings.js');
+const routeReply = (res, code, html) => {
+  return res.writeHead(code, { 'Content-Type': 'text/html' }).end(html)
+}
 
+const routeIdxHtml = `
+  <h1>epiLOL macro generator <span style="font-size:30%;"> by <a href="http://twitter.com/JoshLeaves">JoshLeaves</a></h1>
+  <hr />
+  <br />
+  Url scheme:<br / >
+  - /{face}.png<br />
+  - /{face}%1C{text_top}%1C{text_bot}.png<br />
+  Reminder: ? = %3F<br />
+  <br />
+  Url scheme (for promo 2017):<br />
+  - <a href="/kwame.png">/kwame.png</a><br />
+  - <a href="/roxan%1Cfoo%20bar%20%3F%1CCa%20donne%20soif%20dis%20donc%20!.png">/roxan%1Cfoo%20bar%20%3F%1CCa%20donne%20soif%20dis%20donc%20!.png</a><br />
+  - <a href="/sadirac%1Ccinq%20ans%20pour%1Cdevenir%20un%20robot%20reconnu.png">/sadirac%1Ccinq%20ans%20pour%1Cdevenir%20un%20robot%20reconnu.png</a><br />
+  - <a href="/shawan%1Cmiaou%1Cje%20suis%20un%20chat.png">/shawan%1Cmiaou%1Cje%20suis%20un%20chat.png</a><br />
+  - <a href="/pintade.png">/pintade.png</a><br />
+  - <a href="/jog%1CJ\'ai%20perdu%20mon%20oeil%1CEn%20voyant%20l\'intra%20up%20!.png">/jog%1CJ\'ai%20perdu%20mon%20oeil%1CEn%20voyant%20l\'intra%20up%20!.png</a><br />
+  - [NEW!] <a href="/cyril%1CJ\'aimerais%1CDejeuner en paix%20!.png">/cyril%1CJ\'aimerais%1CDejeuner en paix%20!.png</a><br />
+  <br />
+  Un service <a href="http://epite.ch/">{Epite.ch}</a> &copy; 2011-2013 <b> --- </b> Projet &agrave; but collaboratif. <a href="https://github.com/epite-ch/lol.epite.ch">Fork us!</a>`
+const routeIdx = (res) => { return routeReply(res, 200, routeIdxHtml) }
 
-    function routeIdx (res) {
-        res.writeHead(200, {'Content-Type': 'text/html' });
-        return res.end(strings.routeIdx);
-    };
+const route404Html = `
+  <h1>404! This shit is hosted on the intranet, bitch!</h1>
+  <hr />
+  Seriously dude, I don\'t have what you want here.<br />
+  <br />
+  Just go back to the <a href="/">index</a>, bro!<br />`
+const route404 = (res) => { return routeReply(res, 404, route404Html) }
 
+const route500Html = `
+  <h1>500! It\'s not over 9000 but I failed all the way!</h1><br />
+  <br />
+  Best bet is the server failed. Just hit "retry" or w/ever.<br />
+  <br />
+  If this happens way too much, just contact me: josh DOT guthrie AT gmail DOT com<br />`
+const route500 = (res) => { return routeReply(res, 500, route500Html) }
 
-    function route404 (res, path) {
-        res.writeHead(404, {'Content-Type': 'text/html' });
-        return res.end(strings.route404.replace('%path%', path));
-    };
+module.exports = { routeIdx, route404, route500 }
 
-
-    function route500 (res, path) {
-        res.writeHead(500, {'Content-Type': 'text/html' });
-        return res.end(strings.route500);
-    };
-
-
-    module.exports = {
-        routeIdx: routeIdx,
-        route404: route404,
-        route500: route500
-    };
-
-})();
--- web.js
+++ web.js
@@ -1,83 +1,57 @@
-var http        = require('http');
-var url         = require('url');
-var querystring = require('querystring');
-var imagemagick = require('imagemagick');
-var cookie      = require('cookie');
+const http        = require('http')
+const url         = require('url')
+const imagemagick = require('imagemagick')
+const pictureData = require('./data.js')
+const { routeIdx, route404, route500} = require('./routes')
 
-var data    = require('./data.js');
-var strings = require('./strings.js');
-var routes  = require('./routes.js');
-
-var pictureRegex = /\/([a-z]+)(%1C(.*?)%1C(.*?))?\.png/i;
+const pictureRegex = /\/([a-z]+)(%1C(.*?)%1C(.*?))?\.png/i
 
 function displayPicture (res, path) {
-    var texts = path.match(pictureRegex);
-    texts[1] = texts[1].toLowerCase();
-    if (!data[texts[1]]) return routes.route404(res, path);
+  const texts = path.match(pictureRegex)
+  texts[1] = texts[1].toLowerCase()
+  if (!pictureData[texts[1]]) return route404(res)
 
-    try {
-        texts[3] = (texts[3] ? decodeURIComponent(texts[3]) : data[texts[1]]['t_top']).toUpperCase();
-        texts[4] = (texts[4] ? decodeURIComponent(texts[4]) : data[texts[1]]['t_bot']).toUpperCase();
-    } catch (URIError) {
-        return routes.route404(res, path);
+  try {
+    texts[3] = (texts[3] ? decodeURIComponent(texts[3]) : pictureData[texts[1]]['t_top']).toUpperCase()
+    texts[4] = (texts[4] ? decodeURIComponent(texts[4]) : pictureData[texts[1]]['t_bot']).toUpperCase()
+  } catch (URIError) {
+    return route404(res)
+  }
+
+  const args = [ `pictures/${pictureData[texts[1]]['t_pic']}`,
+    '-font', './Impact.ttf',
+    '-pointSize', '42',
+    '-fill', 'white',
+    '-stroke', 'black',
+    '-strokewidth', '2',
+    '-gravity', 'north', '-annotate', '0', texts[3],
+    '-gravity', 'south', '-annotate', '0', texts[4],
+    'PNG:-'
+  ]
+  return imagemagick.convert(args, (err, stdout, stderr) => {
+    if (err) {
+      console.dir(err)
+      console.dir(stderr)
+      return route500(res)
     }
+    res.writeHead(200, {'Content-Type': 'image/png', 'Content-Length': stdout.length })
+    return res.end(stdout, 'binary')
+  })
+}
 
+const handleServer = (req, res) => {
+  const pathname = url.parse(req.url).pathname
+  if ((pathname.indexOf('/index') === 0) || (pathname === '/')) {
+    return routeIdx(res)
+  }
+  if (pathname === '/favicon.ico') {
+    return res.end('')
+  }
+  if (pictureRegex.test(pathname)) {
+    return displayPicture(res, pathname)
+  }
+  return route404(res)
+}
 
-    var args = [ 'pictures/' + data[texts[1]]['t_pic'],
-        '-font', './Impact.ttf',
-        '-pointSize', '42',
-        '-fill', 'white',
-        '-stroke', 'black',
-        '-strokewidth', '2',
-        '-gravity', 'north', '-annotate', '0', texts[3],
-        '-gravity', 'south', '-annotate', '0', texts[4],
-        'PNG:-'
-    ];
-    return imagemagick.convert(args, pictureDone);
-
-    function pictureDone (err, stdout, stderr) {
-        if (err) {
-            console.dir(err);
-            return routes.route500(res);
-        }
-        res.writeHead(200, {'Content-Type': 'image/png', 'Content-Length': stdout.length });
-        return res.end(stdout, 'binary');
-    };
-};
-
-var analytics   = require('node-ga')(process.env.EPITE_LOL_GA, {
-    custom: {
-        face: function (req, res, next) {
-            var pathname = require('url').parse(req.url).pathname;
-            if (!pictureRegex.test(pathname)) {
-                return next(null, null);
-            }
-            var face = pathname.substr(0, pathname.length - 4).substr(1).split('%1C')[0];
-            return next(null, face);
-        }
-    }
-});
-
-
-
-function handleServer (req, res) {
-    req.cookies = (req.headers.cookie ? cookie.parse(req.headers.cookie) : null);
-    return analytics(req, res, function () {
-        var pathname = url.parse(req.url).pathname;
-        if (pathname.indexOf('/index') === 0) pathname = '/';
-        if (pathname === '/') {
-            return routes.routeIdx(res);
-        }
-        if (pathname === '/favicon.ico') {
-            return res.end('');
-        }
-        if (pictureRegex.test(pathname)) {
-            return displayPicture(res, pathname);
-        }
-        return routes.route404(res, pathname);
-    });
-};
-
-http.createServer(handleServer).listen(process.env.PORT || 3000);
-console.log('Listening!');
+http.createServer(handleServer).listen(process.env.PORT || 5000)
 
